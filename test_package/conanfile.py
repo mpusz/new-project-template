9fdd7a74bd7f12168da6abfb2af986e499e4b838
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-04-24 18:52:54 +0200

    test_package modernization

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from conans import ConanFile, CMake, tools, RunEnvironment
+from conans import ConanFile, CMake, tools
 import os
 
 class TestPackageConan(ConanFile):
@@ -32,21 +32,7 @@ class TestPackageConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
-    def _test_run(self, bin_path):
-        if self.settings.os == "Windows":
-            self.run(bin_path)
-        elif self.settings.os == "Macos":
-            self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-        else:
-            self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
-
     def test(self):
         if not tools.cross_building(self.settings):
-            with tools.environment_append(RunEnvironment(self).vars):
-                self._test_run(os.path.join("bin", "test_package_conan"))
-                self._test_run(os.path.join("bin", "test_package_cmake"))
+            self.run(os.path.join("bin", "test_package_conan"), run_environment=True)
+            self.run(os.path.join("bin", "test_package_cmake"), run_environment=True)
